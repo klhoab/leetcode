16c55946761a3007ea6849f91b4b4967fc9a0d9e
Dickson Ho <klhoab@gmail.com>
2021-08-15 15:07:24 +0800

    Add remarks for dp

--- python/remove_boxes.py
+++ python/remove_boxes.py
@@ -9,16 +9,18 @@ class Solution:
             if mem[i][j][k]:
                 return mem[i][j][k]
             
+            #Case 1: End current color 
             s= i
             while s <= j and boxes[i] == boxes[s]:
                 s+= 1
             cnt = s - i
             ans = (k+ cnt) ** 2 + dp(s, j, 0)
             
+            #Case 2: join to the next current color + dp(s,t-1,0)
             for t in range(s, j+1):
                 if boxes[t]== boxes[i]:
                     ans = max(ans, dp(s, t-1, 0) + dp(t, j, k+ cnt))
-                    break
+                    #break  (fix case 52: cannot assume the first seen of same color is the correct join) 
             
             mem[i][j][k]= ans           
             return ans
