bb03173ccc10bd2f5befb51a5cd630033333c64e
Dickson Ho <klhoab@gmail.com>
2021-08-15 10:22:44 +0800

    Create symmetric-tree.py

---
+++ python/symmetric-tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def sym(root1, root2): 
+            if root1 == None and root2 == None:
+                return True
+            elif root1 == None or root2 == None:
+                return False
+            else:
+                return root1.val== root2.val and sym(root1.left, root2.right) and sym(root1.right, root2.left)
+        
+        return sym(root.left, root.right)
+    
