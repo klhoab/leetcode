28c0173a7c535c71a4d8e1c1a50250fbd604b085
Dickson Ho <klhoab@gmail.com>
2021-08-15 22:37:12 +0800

    Create 3sum.py

---
+++ python/3sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        nums.sort()
+
+        ans = []
+        for i in range(n):
+            for j in range(i+1, n):
+                d = -nums[i] - nums[j]
+                k = bisect_left(nums, d, j+1, n)
+                if k < n and nums[k] == d:
+                    t= sorted([nums[i], nums[j], nums[k]])
+                    if t not in ans:
+                        ans.append(t)
+        return ans
